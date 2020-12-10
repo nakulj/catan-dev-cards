b959d24398c1c3083ed02ee1704bc4f384c7ea04
Nakul Joshi <3213360+nakulj@users.noreply.github.com>
2020-12-09 22:37:51 -0800

    add deck module

---
+++ src/deck.js
@@ -0,0 +1,18 @@
+const shuffle = require('shuffle-array'),
+
+Knight = 'Knight';
+VictoryPoint = 'Victory Point';
+YearOfPlenty = 'Year of Plenty'; 
+Monopoly = 'Monopoly';
+RoadBuilding = 'Road Building';
+
+function make() {
+  return shuffle([
+    ...Array(14).fill(Knight),
+    ...Array(5).fill(VictoryPoint),
+    ...Array(2).fill(YearOfPlenty),
+    ...Array(2).fill(Monopoly),
+    ...Array(2).fill(RoadBuilding),
+  ]);
+}
+module.exports = {Knight, VictoryPoint, YearOfPlenty, Monopoly, RoadBuilding, make};
---
+++ src/deck.test.js
@@ -0,0 +1,11 @@
+const deck = require('./deck.js')
+
+test('constant import', () => {
+  expect(deck.Knight).toBe('Knight');
+});
+
+test('make',()=>{
+  d = deck.make();
+  expect(d.length).toBe(25);
+  expect(d.filter(c=>c == deck.Knight).length).toBe(14);
+});
